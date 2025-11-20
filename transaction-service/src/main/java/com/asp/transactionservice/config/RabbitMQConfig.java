76e6230eeedd0a60d2b4099f0eeecd65ce756f9c
A-Shannidhya <asp45624@gmail.com>
2025-11-20 17:23:48 +0530

    Transaction-Service Fixed

--- transaction-service/src/main/java/com/asp/transactionservice/config/RabbitMQConfig.java
+++ transaction-service/src/main/java/com/asp/transactionservice/config/RabbitMQConfig.java
@@ -1,18 +1,5 @@
 package com.asp.transactionservice.config;
 
-/*
- * Copyright (c) 2025 Ayshi Shannidhya Panda. All rights reserved.
- *
- * This source code is confidential and intended solely for internal use.
- * Unauthorized copying, modification, distribution, or disclosure of this
- * file, via any medium, is strictly prohibited.
- *
- * Project: NPT
- * Author: Ayshi Shannidhya Panda
- * Created on: 17-09-2025
- */
-
-
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -24,28 +11,27 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMQConfig {
 
     @Bean
-    public Jackson2JsonMessageConverter messageConverter(){
+    public Jackson2JsonMessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
-                                         Jackson2JsonMessageConverter jackson2JsonMessageConverter){
-
+                                         Jackson2JsonMessageConverter jackson2JsonMessageConverter) {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
-        rabbitTemplate.convertAndSend(jackson2JsonMessageConverter);
+        // Set the message converter so RabbitTemplate serializes/deserializes JSON correctly
+        rabbitTemplate.setMessageConverter(jackson2JsonMessageConverter);
         rabbitTemplate.setReplyTimeout(5000);
-
+        // IMPORTANT: do NOT send messages here (no convertAndSend)
         return rabbitTemplate;
     }
 
     @Bean
     public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(
             ConnectionFactory connectionFactory,
-            Jackson2JsonMessageConverter jackson2JsonMessageConverter){
+            Jackson2JsonMessageConverter jackson2JsonMessageConverter) {
 
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-
         factory.setConnectionFactory(connectionFactory);
         factory.setMessageConverter(jackson2JsonMessageConverter);
         factory.setConcurrentConsumers(3);
@@ -53,5 +39,66 @@ public class RabbitMQConfig {
 
         return factory;
     }
-
 }
+
+
+
+
+
+//package com.asp.transactionservice.config;
+//
+///*
+// * Copyright (c) 2025 Ayshi Shannidhya Panda. All rights reserved.
+// *
+// * This source code is confidential and intended solely for internal use.
+// * Unauthorized copying, modification, distribution, or disclosure of this
+// * file, via any medium, is strictly prohibited.
+// *
+// * Project: NPT
+// * Author: Ayshi Shannidhya Panda
+// * Created on: 17-09-2025
+// */
+//
+//
+//import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+//import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+//import org.springframework.amqp.rabbit.core.RabbitTemplate;
+//import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class RabbitMQConfig {
+//
+//    @Bean
+//    public Jackson2JsonMessageConverter messageConverter(){
+//        return new Jackson2JsonMessageConverter();
+//    }
+//
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+//                                         Jackson2JsonMessageConverter jackson2JsonMessageConverter){
+//
+//        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+//        rabbitTemplate.convertAndSend(jackson2JsonMessageConverter);
+//        rabbitTemplate.setReplyTimeout(5000);
+//
+//        return rabbitTemplate;
+//    }
+//
+//    @Bean
+//    public SimpleRabbitListenerContainerFactory simpleRabbitListenerContainerFactory(
+//            ConnectionFactory connectionFactory,
+//            Jackson2JsonMessageConverter jackson2JsonMessageConverter){
+//
+//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+//
+//        factory.setConnectionFactory(connectionFactory);
+//        factory.setMessageConverter(jackson2JsonMessageConverter);
+//        factory.setConcurrentConsumers(3);
+//        factory.setMaxConcurrentConsumers(10);
+//
+//        return factory;
+//    }
+//
+//}
